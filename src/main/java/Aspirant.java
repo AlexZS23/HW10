9449e6d07c4d8a20cf629af50d5bf971bbbf5d10
zatoliuk <zatoliuk@cleverstaff.net>
2021-12-11 17:40:12 +0200

    HW10 version 1.0

--- src/main/java/Aspirant.java
+++ src/main/java/Aspirant.java
@@ -1,2 +1,32 @@
-public class Aspirant {
+public class Aspirant extends Student {
+
+    public String scientificWork;
+
+    public Aspirant(String firstName, String lastName, String group, double averageMark, String scientificWork) {
+        super(firstName, lastName, group, averageMark);
+        this.scientificWork = scientificWork;
+    }
+
+    public String getScholarship(double averageMark) {
+
+        if (averageMark == 5) {
+            return "200 грн.";
+        } else {
+            return "180 грн.";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Олег", "Ковалёв", "Менеджмент", 5);
+        Student student2 = new Student("Иван", "Забарный", "Логистика", 4.5);
+        Student aspirant1 = new Aspirant("Григорий", "Королёв", "Информатика", 5, "Технологии виртуальной реальности");
+        Student aspirant2 = new Aspirant("Лев", "Яшин", "Спорт", 4.9, "Тактика в футболе");
+
+        Student[] studentsAndAspirants = {student1, student2, aspirant1, aspirant2};
+
+        for (Student students : studentsAndAspirants) {
+            System.out.println("Стипендия для " + students.lastName + " " + students.firstName + " составляет " + students.getScholarship(students.averageMark));
+        }
+    }
 }
--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -2,4 +2,22 @@ public class Cat extends Animal {
 
 //    Dog, Cat, Horse переопределяют методы makeNoise, eat.
 //    Добавьте переменные в классы Dog, Cat, Horse, характеризующие только этих животных.
+
+    public int countOfhitChristmassTree;
+
+    public Cat(String food, String location, String nickName, int countOfhitChristmassTree) {
+        super(food, location, nickName);
+        this.countOfhitChristmassTree = countOfhitChristmassTree;
+    }
+
+
+    public void makeNoise() {
+        System.out.println("Pay attention!!! Кот " + nickName + " может шуметь по ночам.");
+    }
+
+    public void eat() {
+        System.out.println("Кот любит кушать " + food);
+    }
+
+
 }
